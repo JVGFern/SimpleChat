79e3f67653624d5a813a92776b57bded1ba9664d
JVGFern <jvgf.br@gmail.com>
2023-03-24 18:34:16 -0300

    Fix: add commands

--- index.js
+++ index.js
@@ -3,87 +3,79 @@ const chatInput = document.getElementById('mensageInput');
 const chatButton = document.querySelector('.chat-input button');
 const userTypeSelect = document.getElementById('userTypeSelect');
 
-const messageTypeWidth = userTypeSelect.value;
-if(messageTypeWidth === "Atendente"){
-    userTypeSelect.style.width = "16%";
+let messageTypeWidth = userTypeSelect.value;
+setUserTypeSelectWidth(messageTypeWidth);
+
+function setUserTypeSelectWidth(type) {
+  const width = type === 'Atendente' ? '16%' : '13%';
+  userTypeSelect.style.width = width;
 }
-else{
-    userTypeSelect.style.width = "13%";
-}
+
 function addMessage(author, message, isAgent) {
+  const messageContainer = document.createElement('div');
+  messageContainer.className = 'message';
 
-    const messageContainer = document.createElement('div');
-    messageContainer.className = 'message';
-    let x = '';
-
-    if(isAgent === false){
-        x = 'authoruser';
-    }
-    else{
-        x = 'author';
-    };
-
-    const authorElement = document.createElement('span');
-    authorElement.className = x;
-    authorElement.textContent = `${author} diz:`;
-    messageContainer.appendChild(authorElement);
+  const authorClass = isAgent ? 'author' : 'authoruser';
+  const authorElement = document.createElement('span');
+  authorElement.className = authorClass;
+  authorElement.textContent = `${author} diz:`;
+  messageContainer.appendChild(authorElement);
 
 
-    const messageContent = document.createElement('div');
-    messageContent.className = `message-content ${isAgent ? 'from-agent' : 'from-user'} `;
-    messageContent.id = `${isAgent ? 'from-agent' : 'from-user'}`;
+  const messageContent = document.createElement('div');
+  messageContent.className = `message-content ${isAgent ? 'from-agent' : 'from-user'} `;
+  messageContent.id = `${isAgent ? 'from-agent' : 'from-user'}`;
 
-    const textElement = document.createElement('span');
-    textElement.className = 'text';
-    textElement.textContent = message;
-    messageContent.appendChild(textElement);
+  const textElement = document.createElement('span');
+  textElement.className = 'text';
+  textElement.textContent = message;
+  messageContent.appendChild(textElement);
 
-    const timeElement = document.createElement('span');
-    timeElement.className = 'time';
-    const timeNow = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    timeElement.textContent = timeNow;
-    messageContent.appendChild(timeElement);
+  const timeElement = document.createElement('span');
+  timeElement.className = 'time';
+  const timeNow = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  timeElement.textContent = timeNow;
+  messageContent.appendChild(timeElement);
 
-    messageContainer.appendChild(messageContent);
-    chatMessages.appendChild(messageContainer);
+  messageContainer.appendChild(messageContent);
+  chatMessages.appendChild(messageContainer);
 }
 
-chatButton.addEventListener('click', () => {
-    sendMessage();
+chatButton.addEventListener('click', sendMessage);
+userTypeSelect.addEventListener('change', (event) => {
+  const type = event.target.value;
+  setUserTypeSelectWidth(type);
 });
-
-userTypeSelect.addEventListener('click', () => {
-    const messageTypeWidth = userTypeSelect.value;
-    if(messageTypeWidth === "Atendente"){
-        userTypeSelect.style.width = "16%";
-    }
-    else{
-        userTypeSelect.style.width = "13%";
-    }
-});
-
 
 chatInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        sendMessage();
-    };
+  if (event.key === 'Enter') {
+    sendMessage();
+  }
 });
 
 function sendMessage() {
-    const messageType = userTypeSelect.value;
-    const message = chatInput.value.trim();
-    if (message) {
-        let y = false;
-        if(messageType === 'Atendente'){
-            y = true;
-        }
-        else{
-            y = false;
-            
-        }
-        addMessage(messageType, message, y);
-        chatInput.value = '';
-    };
+  const messageType = userTypeSelect.value;
+  const message = chatInput.value.trim();
+  if (!message) {
+    return;
+  }
+  const isAgent = messageType === 'Atendente';
+  addMessage(messageType, message, isAgent);
+  chatInput.value = '';
+  if (message === "/clear") {
+    const messages = document.querySelectorAll('.message');
+    messages.forEach((message) => message.remove());
+    chatInput.value = '';
+  }
+  if (message === "/reset") {
+    const messages = document.querySelectorAll('.message');
+    messages.forEach((message) => message.remove());
+    chatInput.value = '';
+    addMessage('Atendente', 'Bom diaa!!!', true);
+    addMessage('Atendente', 'Olá, como posso ajudá-lo?', true);
+    addMessage('Você', 'Gostaria de saber mais sobre o produto X.', false);
+    addMessage('Você', 'E também gostaria de saber o valor do produto X.', false) 
+  }
 };
 
 
